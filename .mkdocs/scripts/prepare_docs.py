b713a2c2d8f09004fe645a4bb52f31b656d77572
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-09-30 15:55:35 +0900

    mkdocsとgithub actionsでの自動公開

---
+++ .mkdocs/scripts/prepare_docs.py
@@ -0,0 +1,37 @@
+import yaml
+import shutil
+import os
+
+PREFERENCE_DIR = ".mkdocs"
+DOCKS_DIR = "docs"
+
+os.makedirs(DOCKS_DIR, exist_ok=True)
+
+# 参考元
+# https://tick-taku.com/blog/obsidian_mkdocs_githubactions_netlify_snk/#mkdocs-html
+with open(f"{PREFERENCE_DIR}/conf.yml") as conf_file:
+    conf = yaml.safe_load(conf_file)
+    shutil.copy(f"{PREFERENCE_DIR}/mkdocs.yml", "./")
+
+    if "index" in conf:
+        index = conf["index"]
+        shutil.copy(index, f"{DOCKS_DIR}/{os.path.basename(index)}")
+
+    if "logo" in conf:
+        logo = conf["logo"]
+        shutil.copy(logo, f"{DOCKS_DIR}/{os.path.basename(logo)}")
+
+    if "favicon" in conf:
+        favicon = conf["favicon"]
+        shutil.copy(favicon, f"{DOCKS_DIR}/{os.path.basename(favicon)}")
+
+    if "custom_theme" in conf:
+        theme = conf["custom_theme"]
+        shutil.copytree(f"{PREFERENCE_DIR}/{theme}", theme, dirs_exist_ok=True)
+
+    for doc in conf["docs"]:
+        shutil.copytree(doc, f"{DOCKS_DIR}/{doc}", dirs_exist_ok=True)
+
+    if "files" in conf:
+        for file in conf["files"]:
+            shutil.copytree(file, f"{DOCKS_DIR}/{os.path.basename(file)}")
